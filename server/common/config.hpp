90bb9ee3fe7463f565ab231799a669efbca7ca29
mbukhta <nikita.bukhta.dev@gmail.com>
2024-05-14 18:55:39 +0300

    [server][config]
    changed pass

--- server/common/config.hpp
+++ server/common/config.hpp
@@ -6,7 +6,7 @@ namespace server::common::config {
 const std::string   LOCALHOST   {"127.0.0.1"};
 const std::string   DB_HOST     {LOCALHOST};
 const std::string   DB_USERNAME {"mbukhta"};
-const std::string   DB_PASSWORD {"NikitaBukhta17110313"};
+const std::string   DB_PASSWORD {"1qa@WS3ed"};
 const std::string   DB_NAME     {"polyclinic"};
 const std::uint16_t DB_PORT {5432};
 
